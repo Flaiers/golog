29f9132f46a794c8570dd2e21c07e8ff3be087c3
Max <bigin.maks@gmail.com>
2021-08-26 19:37:10 +0300

    add redis server and test

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,12 @@
 import requests
+import redis
 
 
 request = requests.get('http://0.0.0.0')
-print(request.json())  # {'error': False}
+json = request.json()
+print(json)  # {'error': False}
+
+client = redis.Redis(host='localhost', port=6379, password='redis_pass')
+client.set('error', str(json.get('error')))
+value = client.get('error')
+print(value.decode())  # False
