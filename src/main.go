8b3b4e496d19953a737272a1cc71769bd59e36ba
Max <bigin.maks@gmail.com>
2021-08-26 22:15:05 +0300

    connect to redis

--- src/main.go
+++ src/main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/go-redis/redis"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+var ctx = context.Background()
+
+var rdb = redis.NewClient(&redis.Options{
+	Addr:     "localhost:6379",
+	Password: os.Getenv("REDIS_PASSWORD"),
+	DB:       0,
+})
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`{"error": false}`))
