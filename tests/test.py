d5ab5cba0fc17d1005d19f51a2db0ff4be4a71e4
Max <bigin.maks@gmail.com>
2021-08-26 22:38:29 +0300

    get redis password in .env

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,17 @@
+from dotenv import load_dotenv
+
 import requests
 import redis
+import os
 
+
+load_dotenv()
 
 request = requests.get('http://0.0.0.0')
 json = request.json()
-print(json)  # {'error': False}
+print(json)  # {'error': False, 'data': 'OK'}
 
-client = redis.Redis(host='localhost', port=6379, password='redis_pass')
-client.set('error', str(json.get('error')))
-value = client.get('error')
-print(value.decode())  # False
+client = redis.Redis(host='localhost', port=6379, password=os.environ.get('REDIS_PASSWORD'))
+client.set('data', json.get('data'))
+value = client.get('data')
+print(value.decode())  # OK
