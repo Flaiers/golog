d47b9c635eb328e7a967e4cd8f99a4249dc4484a
Max <bigin.maks@gmail.com>
2021-08-27 23:21:04 +0300

    fix load env, and set key-value redis

--- tests/test.py
+++ tests/test.py
@@ -16,9 +16,8 @@ data = {
 
 request = requests.post('https://analytics.fla.codes/', json=data)
 json = request.json()
-print(json)  # {'error': False, 'data': 'OK'}
+print(json)  # {'error': False, 'data': 'ok'}
 
 client = redis.Redis(host='localhost', port=6379, password=os.environ.get('REDIS_PASSWORD'))
-client.set('data', json.get('data'))
 value = client.get('data')
-print(value.decode())  # OK
+print(value.decode())  # {"date":"","url":"https://google.com/","method":"GET","status":200,"user":"bigin.maks@gmail.com","headers":"","body":"","comment":""}
