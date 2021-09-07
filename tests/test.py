37f28d3c54c59f7363014d55bf2d0201883f7ccd
Max <bigin.maks@gmail.com>
2021-09-07 19:39:33 +0300

    update test case

--- tests/test.py
+++ tests/test.py
@@ -2,21 +2,45 @@ from dotenv import load_dotenv
 from datetime import datetime
 
 import requests
+import psycopg2
 import os
 
 
 load_dotenv()
 
-data = {
-    'date': str(datetime.now()),
-    'url': 'https://fla.codes/',
-    'method': 'GET',
-    'status': 404,
-    'user_id': 2,
-    'body': None,
-    'comment': 'test',
-}
 
-request = requests.post('https://logging.fla.codes/', json=data)
-json = request.json()
-print(json)  # {'error': False, 'data': 'ok'}
+def send_test(url):
+    data = {
+        'date': str(datetime.now()),
+        'url': 'https://fla.codes/',
+        'method': 'GET',
+        'status': 404,
+        'user_id': 2,
+        'body': None,
+        'comment': 'test',
+    }
+
+    request = requests.post(url, json=data)
+    json = request.json()
+    print(json)  # {'error': False, 'data': 'ok'}
+
+
+def psql_test(dsn):
+    connection = psycopg2.connect(dsn)
+    cursor = connection.cursor()
+
+    query = '''
+        SELECT COUNT(*)
+        FROM logging;
+    '''
+
+    cursor.execute(query)
+    for id in cursor.fetchone():
+        print(id)
+
+    cursor.close()
+    connection.close()
+
+
+send_test('https://logging.fla.codes/')
+psql_test(os.getenv('DB_DSN'))
